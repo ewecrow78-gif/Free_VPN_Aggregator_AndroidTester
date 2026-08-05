5a5739cdfaf00476af30443cfcf35e0a1d6fe8b9
Gh0st <ewecrow78@gmail.com>
2026-08-05 22:29:22 +0600

    fix: AGP 8.3, fix androidResources, fix plugin versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,17 +21,24 @@ android {
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
     kotlinOptions {
         jvmTarget = "17"
     }
-    
-    // We will place xray in assets, so it doesn't get compressed
-    aaptOptions {
-        noCompress("xray")
+
+    // AGP 8.x: use androidResources instead of deprecated aaptOptions
+    androidResources {
+        noCompress += listOf("xray")
+    }
+
+    packaging {
+        // Keep xray binary uncompressed in the APK
+        jniLibs.keepDebugSymbols += "**/xray"
     }
 }
 
@@ -39,10 +46,10 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
-    
+
     // Networking
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    
+
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
